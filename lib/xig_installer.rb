025ffc2c3f8f218961f817f61058edd8b26bd90d
wtnabe <wtnabe@gmail.com>
2010-01-05 11:17:22 +0900

    first version

---
+++ lib/xig_installer.rb
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+require 'pathname'
+require 'optparse'
+require 'erb'
+include FileUtils::Verbose
+
+class XigInstaller
+  VERSION    = '0.0.1'
+  TARGET_DIR = '/usr/local/bin'
+  XIG_GLOB   = '*ig.rb'
+
+  class CommandNotExist < StandardError ; end
+  class DirectoryNotExist < StandardError; end
+  class NetIRCNotInstalled < StandardError; end
+  class TargetGatewayNotValid < StandardError; end
+  class TargetGatewayNotInstalled < StandardError; end
+
+  def initialize
+    @commands           = nil
+    @gateways           = nil
+    @gateway_dir        = nil
+    @max_cmdname_length = nil
+    @target_dir         = TARGET_DIR
+  end
+  attr_reader :target_dir
+
+  def run
+    if ( cmd = parse_args() )
+      if ( commands.include?( cmd ) )
+        exec_cmd( cmd )
+      elsif ( cmd == 'help' )
+        usage( ARGV.shift )
+      elsif ( cmd =~ /\A(?:--)?version\z/ )
+        usage( 'version' )
+      end
+    else
+      usage
+    end
+  end
+
+  def exec_cmd( cmd )
+    if commands.include?( cmd )
+      send( "cmd_#{cmd}", *ARGV )
+    else
+      raise CommandNotExist
+    end
+  end
+
+  def commands
+    if ( !@commands )
+      @commands = methods.grep( /\Acmd_(.*)\z/ ).map { |e|
+        e.sub( /\Acmd_(.*)\z/, '\1' )
+      }
+    end
+
+    return @commands
+  end
+
+  def installed?( name )
+    Dir.chdir( @target_dir ) {
+      Dir.glob( XIG_GLOB ).include?( name )
+    }
+  end
+
+  def different?( name )
+    begin
+      src = File.stat( File.join( gateway_dir, name ) )
+      begin
+        dest = File.stat( File.join( @target_dir, name ) )
+        if ( src.mtime != dest.mtime )
+          return true
+        else
+          return false
+        end
+      rescue Errno::ENOENT
+        return true
+      end
+    rescue Errno::ENOENT
+      raise NetIRCNotInstalled
+    end
+  end
+
+  def gateways( dir = nil )
+    dir ||= gateway_dir
+
+    if ( dir )
+      if ( File.exist?( dir ) )
+        Dir.chdir( dir ) {
+          Dir.glob( XIG_GLOB )
+        }
+      else
+        raise DirectoryNotExist, dir
+      end
+    else
+      raise NetIRCNotInstalled
+    end
+  end
+
+  def gateway_dir
+    if ( @gateway_dir.nil? )
+      net_irc = `gem which net/irc`.chomp
+
+      if ( File.exist?( net_irc ) )
+        @gateway_dir = (Pathname( net_irc ).
+                         parent.parent.parent + 'examples').to_s
+      else
+        @gateway_dir = false
+      end
+    end
+
+    return @gateway_dir
+  end
+
+  def cmd_list( subcmd = nil )
+    subcmds = methods.grep( /\A_cmd_list_(.*)\z/ ).map { |c|
+      c.sub( /\A_cmd_list_(.*)\z/, '\1' )
+    }
+    if ( subcmd == 'help' )
+      subcmds
+    elsif ( subcmds.include?( subcmd ) )
+      puts send( "_cmd_list_#{subcmd}" )
+    else
+      puts _cmd_list_available
+    end
+  end
+
+  def _cmd_list_available
+    gateways
+  end
+
+  def _cmd_list_installed
+    gateways.reject { |e|
+      !installed?( e )
+    }
+  end
+
+  def _cmd_list_update
+    _cmd_list_installed.reject { |e|
+      different?( name )
+    }
+  end
+
+  def cmd_install( *targets )
+    available = gateways & targets
+    if ( targets.size == 0 )
+      _copy( gateways )
+    elsif ( available == targets )
+      _copy( targets )
+    else
+      raise TargetGatewayNotValid, (targets - available).join( ', ' )
+    end
+  end
+
+  def cmd_upgrade( *targets )
+    if ( targets.size == 0 )
+      _cmd_list_installed.map { |e|
+        different?( e ) ? cmd_install( e ).to_s : nil
+      }.compact
+    else
+      targets.reject { |e|
+        !installed?( e )
+      }.map { |e|
+        different?( e ) ? cmd_install( e ).to_s : nil
+      }.compact
+    end
+  end
+
+  def _copy( targets )
+    targets.each { |e|
+      cp( File.join( gateway_dir, e ), @target_dir, :preserve => true )
+    }
+  end
+
+  def cmd_uninstall( *targets )
+    installed = _cmd_list_installed
+    available = installed & targets
+    if ( targets.size == 0 )
+      _remove( installed )
+    elsif ( available == targets )
+      _remove( targets )
+    else
+      raise TargetGatewayNotInstalled, (targets - installed).join( ', ' )
+    end
+  end
+
+  def _remove( targets )
+    targets.each { |e|
+      rm( File.join( @target_dir, e ) )
+    }
+  end
+
+  def usage( cmd = nil )
+    puts "#{self.class} ver.#{VERSION}"
+    if ( commands.include?( cmd ) )
+      puts "\n"
+      puts send( "usage_#{cmd}" )
+    elsif ( cmd == 'version' )
+      ;
+    else
+      puts ERB.new( <<EOD, nil, '-' ).result( binding )
+
+Usage: #{File.basename( $0 )} command [options] [TARGET]
+
+Commands:
+<%- commands.sort.each do |e| -%>
+  <%= simple_help( e ) %>
+<%- end -%>
+
+Options:
+<%= parser.help.sub( /[\r\n]/, '' ) %>
+EOD
+    end
+    exit
+  end
+
+  def usage_list( subcmd = nil )
+    ERB.new( <<EOD, nil, '-' ).result( binding )
+show gateway list
+
+Usage: #{File.basename( $0 )} list [subcommand]
+
+Subcommands:
+<%- cmd_list( 'help' ).sort.each do |cmd| -%>
+  <%= cmd %>
+<%- end -%>
+EOD
+  end
+
+  def usage_install
+    ERB.new( <<EOD ).result( binding )
+install specified or all gateways
+EOD
+  end
+
+  def usage_upgrade
+    ERB.new( <<EOD ).result( binding )
+upgrade specified or all gateways if need
+EOD
+  end
+
+  def usage_uninstall
+    ERB.new( <<EOD ).result( binding )
+uninstall specified or all gateways
+EOD
+  end
+
+  def simple_help( cmd )
+    if ( commands.include?( cmd ) )
+      return sprintf( "%-*s   %s",
+                      max_cmdname_length,
+                      cmd,
+                      send( "usage_#{cmd}" ).lines.first.chomp )
+    end
+  end
+
+  def max_cmdname_length
+    if ( !@max_cmdname_length )
+      @max_cmdname_length = commands.map { |e|
+        e.size
+      }.max
+    end
+
+    return @max_cmdname_length
+  end
+
+  def parse_args
+    parser.parse!( ARGV )
+    return ARGV.shift
+  end
+
+  def parser
+    return OptionParser.new { |opt|
+      opt.banner = ''
+      opt.on( '-t', '--target DIR', 'target directory' ) { |d|
+        dir = File.expand_path( d )
+        if ( File.exist?( dir ) and File.directory?( dir ) )
+          @target_dir = dir
+        else
+          raise DirectoryNotExist, d
+        end
+      }
+      opt.on( '-g', '--gateway DIR', 'gateway source directory' ) { |d|
+        dir = File.expand_path( d )
+        if ( File.exist?( dir ) and File.directory?( dir ) )
+          @gateway_dir = dir
+        else
+          raise DirectoryNotExist, d
+        end
+      }
+    }
+  end
+end
+
+if ( !String.respond_to?( :lines ) )
+  class String
+    def lines
+      self.split( /(?:\r\n|[\r\n])/ )
+    end
+  end
+end
---
+++ test/xig_installer_test.rb
@@ -0,0 +1,155 @@
+require 'rubygems'
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+require "test/unit"
+class XigInstallerTest < Test::Unit::TestCase
+  def setup
+    @obj = XigInstaller.new
+    begin
+      @obj.cmd_uninstall
+    rescue
+    end
+  end
+
+  def teardown
+    @obj.cmd_uninstall
+  end
+
+  def test_exec_cmd
+    c = 'abc'
+    assert( !@obj.commands.include?( c ) )
+    begin
+      assert( @obj.exec_cmd( c ) )
+    rescue XigInstaller::CommandNotExist
+      assert( true )
+    end
+  end
+
+  def test_commands
+    cmds = @obj.commands
+    assert( cmds.is_a?( Array ) )
+    assert( cmds.size > 0 )
+  end
+
+  def test_installed?
+    assert( !@obj.installed?( 'tig.rb' ) )
+    @obj.cmd_install( 'tig.rb' )
+    assert( @obj.installed?( 'tig.rb' ) )
+  end
+
+  def test_gateways
+    begin
+      require 'net/irc'
+      gateways = @obj.gateways
+      assert( gateways.is_a?( Array ) )
+      assert( gateways.size > 0 )
+      gateways.each { |e|
+        assert( e =~ /\A.*ig\.rb\z/ )
+      }
+    rescue LoadError
+      assert( !@obj.gateways )
+    end
+    begin
+      @obj.gateways( 'target' )
+    rescue XigInstaller::DirectoryNotExist
+      assert( true )
+    end
+  end
+
+  def test_gateway_dir
+    begin
+      require 'net/irc'
+      assert( File.exist?( @obj.gateway_dir ) )
+    rescue LoadError
+      assert( !@obj.gateway_dir )
+    end
+  end
+
+  def test_cmd_list
+    subcmds = @obj.cmd_list( 'help' )
+    assert( subcmds.is_a?( Array ) )
+    assert( subcmds.size > 0 )
+  end
+
+  def test_cmd_list_available
+    availables = @obj._cmd_list_available
+    assert( availables.is_a?( Array ) )
+    assert( availables.size > 0 )
+  end
+
+  def test_cmd_list_installed
+    assert( @obj._cmd_list_installed.is_a?( Array ) )
+  end
+
+  def test_cmd_list_update
+    installed = @obj._cmd_list_installed
+    assert( @obj._cmd_list_update.size == installed.size )
+  end
+
+  def test_cmd_install
+    begin
+      @obj.cmd_install( 'foo' )
+    rescue XigInstaller::TargetGatewayNotValid
+      assert( true )
+    end
+    assert( tig = @obj.cmd_install( 'tig.rb' ) )
+    @obj.cmd_uninstall()
+    assert( all = @obj.cmd_install() )
+    assert( all.size > tig.size )
+  end
+
+  def test_cmd_upgrade
+    target = 'tig.rb'
+    @obj.cmd_install( target )
+    File.utime( File.stat( __FILE__ ).atime,
+                File.stat( __FILE__ ).mtime,
+                File.join( @obj.target_dir, target ) )
+    assert( @obj.different?( target ) )
+    assert( @obj.cmd_upgrade( target ) == [target] )
+    assert( !@obj.different?( target ) )
+    assert( @obj.cmd_upgrade( target ) == [] )
+    File.utime( File.stat( __FILE__ ).atime,
+                File.stat( __FILE__ ).mtime,
+                File.join( @obj.target_dir, target ) )
+    assert( @obj.cmd_upgrade() == [target] )
+  end
+
+  def test_cmd_uninstall
+    @obj.cmd_install( 'tig.rb' )
+    assert( tig = @obj.cmd_uninstall( 'tig.rb' ) )
+    @obj.cmd_install( 'tig.rb' )
+    assert( all = @obj.cmd_uninstall() )
+    assert( tig == all )
+    assert( @obj.cmd_uninstall() )
+    begin
+      @obj.cmd_uninstall( 'tig.rb' )
+    rescue XigInstaller::TargetGatewayNotInstalled
+      assert( true )
+    end
+    begin
+      @obj.cmd_uninstall( 'foo' )
+    rescue XigInstaller::TargetGatewayNotInstalled
+      assert( true )
+    end
+   end
+  def test_parser
+    assert( @obj.parser.parse( %w( help ) ) == %w( help ) )
+    assert( @obj.target_dir == '/usr/local/bin' )
+    # -t
+    target = (%w( /opt/local/bin /sw/local/bin ) +
+              [File.expand_path(File.dirname( __FILE__ ))]).find { |e|
+      File.exist?( e )
+    }
+    @obj.parser.parse( %w( install -t ) + [target] )
+    assert( @obj.target_dir == target )
+    begin
+      @obj.parser.parse( %w( install -t ) + [target] )
+    rescue XigInstaller::DirectoryNotExist
+      assert( true )
+    end
+    # -g
+    src = (Pathname( __FILE__ ).parent.expand_path).to_s
+    @obj.parser.parse( %w( install -g ) + [src] )
+    assert( @obj.gateway_dir == src )
+  end
+end
